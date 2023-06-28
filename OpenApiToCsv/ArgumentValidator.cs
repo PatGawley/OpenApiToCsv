ac21bf52be4744b5412cecc0cd7aa4d51073be39
Patrick Gawley <ptl92671@openwork.uk.com>
2023-06-28 14:13:13 +0100

    Completed argument validator

--- OpenApiToCsv/ArgumentValidator.cs
+++ OpenApiToCsv/ArgumentValidator.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace OpenApiToCsv
 {
@@ -12,9 +14,21 @@ namespace OpenApiToCsv
     }
     public class ArgumentValidator : IArgumentValidator
     {
+        private const string filenameRegex = "^[a-z0-9A-Z]{1,10}.json$";
+
         public (bool, string[]?) IsValid(string[] arguments)
         {
-            throw new NotImplementedException();
+            
+            if(arguments.Length != 1) {
+                return (false, new string[] { "No arguments passed - 1 required" });
+            }
+
+            if (!Regex.IsMatch(arguments[0], filenameRegex))
+            {
+                return (false, new string[] { $"Argument must be 10 digit json filename conforming to Regex - {filenameRegex}" });
+            }
+            
+            return (true,null);
         }
     }
 }
--- TestOpenApiToCsv/ArgumentValidatorTests.cs
+++ TestOpenApiToCsv/ArgumentValidatorTests.cs
@@ -1,20 +1,89 @@
 using OpenApiToCsv;
+using FluentAssertions;
 
 namespace TestOpenApiToCsv
 {
     public class ArgumentValidatorTests
     {
-        private IArgumentValidator validator;
+        private readonly IArgumentValidator validator = new ArgumentValidator();
 
-        ArgumentValidatorTests()
-        {
-            validator = new ArgumentValidator();
-        }
 
         [Fact]
-        public void GivenNoArguments_WhenIsValid_ThenFalseAndIncorrectNumberOfArguments()
+        public void GivenNoArguments_WhenIsValid_ThenFalseAndNoArgumentsPassed()
         {
-            var givenNoArguments
+            
+            string[] givenNoArguments = { };
+
+            (bool whenIsValid,var results) = validator.IsValid(givenNoArguments);
+
+            whenIsValid.Should().BeFalse();
+            string? andNoArgumentsPassed = results?[0];
+            andNoArgumentsPassed.Should().Be("No arguments passed - 1 required");
+
+        }
+        [Fact]
+        public void GivenTooManyArguments_WhenIsValid_ThenFalseAndTooManyArgumentsPassed()
+        {
+            string[] givenNoArguments = {"","" };
+
+            (bool whenIsValid, var results) = validator.IsValid(givenNoArguments);
+
+            whenIsValid.Should().BeFalse();
+            string? andNoArgumentsPassed = results?[0];
+            andNoArgumentsPassed.Should().Be("No arguments passed - 1 required");
+        }
+        [Theory]
+        [InlineData("testfile.json")]
+        [InlineData("1.json")]
+        [InlineData("12.json")]
+        [InlineData("123.json")]
+        [InlineData("1234.json")]
+        [InlineData("12345.json")]
+        [InlineData("123456.json")]
+        [InlineData("1234567.json")]
+        [InlineData("12345678.json")]
+        [InlineData("1234567890.json")]
+        [InlineData("asdfghjklo.json")]
+        [InlineData("hsliewneks.json")]
+        [InlineData("SHSLIEHLS.json")]
+        [InlineData("FILE1.json")]
+        [InlineData("File1.json")]
+        public void GivenOneArgumentCorrectlyFormatted_WhenIsValid_ThenTrueAndNoResult(string validFilename)
+        {
+            string[] givenOneArgumentCorrectlyFormatted = { validFilename };
+
+            (bool whenIsValid, var results) = validator.IsValid(givenOneArgumentCorrectlyFormatted);
+
+            whenIsValid.Should().BeTrue();
+            string? andNoArgumentsPassed = results?[0];
+            andNoArgumentsPassed.Should().BeNull();
+        }
+        [Theory]
+        [InlineData("testfile.txt")]
+        [InlineData("testfile.json")]
+        [InlineData("£1.json")]
+        [InlineData("£12.json")]
+        [InlineData(".123.json")]
+        [InlineData("!1234.json")]
+        [InlineData("@12345.json")]
+        [InlineData("~123456.json")]
+        [InlineData("#1234567.json")]
+        [InlineData(" 12345678.json")]
+        [InlineData("123 4567890.json")]
+        [InlineData("as  dfghjklo.json")]
+        [InlineData("hsli-ewneks.json")]
+        [InlineData("SHSL_IEHLS.json")]
+        [InlineData("FILE1.jsn")]
+        [InlineData("File1json")]
+        public void GivenOneArgumentInCorrectlyFormatted_WhenIsValid_ThenFalseAndInvalidFileName(string invalidFilename)
+        {
+            string[] givenOneArgumentInCorrectlyFormatted = { "testfile.txt" };
+
+            (bool whenIsValid, var results) = validator.IsValid(givenOneArgumentInCorrectlyFormatted);
+
+            whenIsValid.Should().BeFalse();
+            string? andNoArgumentsPassed = results?[0];
+            andNoArgumentsPassed.Should().Be("Argument must be 10 digit json filename conforming to Regex - ^[a-z0-9A-Z]{1,10}.json$");
         }
     }
 }
